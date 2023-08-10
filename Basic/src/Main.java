82bf22c91b46844605f001acc5e0a65deeeea880
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-08-10 21:57:52 +0700

    Update Main.java

--- Basic/src/Main.java
+++ Basic/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Initial project.");
+		int x = 123;
+		double y = 3.41;
+		boolean z = true;
+		char symbol = '@';
+		String name = "Bro";
+		
+		System.out.println("Hello World");
+		System.out.println("From AdopPix !");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(symbol);
+		System.out.println(name);
 	}
 }
