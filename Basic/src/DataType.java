b43c72564c28482dc1f5f08a5bcad1ac901d17b3
Colorfuls <bike.chanokchon2001@gmail.com>
2023-08-11 14:38:43 +0700

    add variable

---
+++ Basic/src/DataType.java
@@ -0,0 +1,13 @@
+
+public class DataType {
+	public static void main(String[] args) {
+		int a = 5;
+		long b = 123123123123123123L;
+		byte c = 1;
+		float d = 3.14f;
+		double e = 3.14d;
+		boolean f = false;
+		char g = 'a';
+		String h = "Hello WOrld";
+	}
+}
