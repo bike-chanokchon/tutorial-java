ade3fd16e434f4034abc59c789f218ded4b825fe
Colorfuls <bike.chanokchon2001@gmail.com>
2023-08-11 15:11:19 +0700

    Create Expression.java

---
+++ Basic/src/Expression.java
@@ -0,0 +1,14 @@
+
+public class Expression {
+
+	public static void main(String[] args) {
+		// expression = operands & operators
+		// operands = values, variable, numbers, quantity
+		// operators = + - * / %
+		
+		int friends = 10;
+		friends = friends * 2;
+		System.out.println("friends");
+	}
+
+}
