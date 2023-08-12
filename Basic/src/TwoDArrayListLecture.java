6887d44b9ac8c61aa799f6c861bce255c729f9ec
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-08-13 01:07:43 +0700

    2d arraylist lecture

--- Basic/src/TwoDArrayListLecture.java
+++ Basic/src/TwoDArrayListLecture.java
@@ -1,6 +1,26 @@
+import java.util.*;
 
 public class TwoDArrayListLecture {
 	public static void main(String[] args) {
+		ArrayList<ArrayList<String>> groceries = new ArrayList<ArrayList<String>>();
 		
+		ArrayList<String> bakeries = new ArrayList<String>();
+		bakeries.add("pasta");
+		bakeries.add("garlic bread");
+		bakeries.add("donut");
+		groceries.add(bakeries);
+		
+		ArrayList<String> products = new ArrayList<String>();
+		products.add("tomato");
+		products.add("zucchini");
+		products.add("peppers");
+		groceries.add(products);
+		
+		ArrayList<String> drinks = new ArrayList<String>();
+		drinks.add("soda");
+		drinks.add("coffee");
+		groceries.add(drinks);
+		
+		System.out.println(groceries);
 	}
 }
