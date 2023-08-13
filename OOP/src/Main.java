7ecc4366e83ab5bf25e6b51277749fa6a01a35b2
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-08-14 01:53:31 +0700

    access modifier lecture

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
 	public static void main(String[] args) {
-		Car3 car3 = new Car3();
-		car3.go();
+
 	}
 }
---
+++ OOP/src/package2/C.java
@@ -0,0 +1,8 @@
+package package2;
+
+public class C {
+	public String publicMessage = "This is public";
+	protected String protectedMessage = "This is protected";
+	private String privateMessage = "This is private";
+	String defaultMessage = "This is the default";
+}
